6dce205c62309256b0d30daa74a1239ed6bc71a5
Fernandes Mukanda <fernandezmukanda@gmail.com>
2025-09-13 11:19:44 +0300

    Update Java.ts

--- 0x04-TypeScript/task_4/js/subjects/Java.ts
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -1,1 +1,23 @@
+/// <reference path="Subject.ts" />
+/// <reference path="Teacher.ts" />
 
+namespace Subjects {
+  // Declaration merging - add experienceTeachingJava to Teacher interface
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class Java extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Java';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher.experienceTeachingJava && this.teacher.experienceTeachingJava > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return 'No available teacher';
+      }
+    }
+  }
+}

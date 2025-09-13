3a6effeca5157179c985cd921785c93859d68391
Fernandes Mukanda <fernandezmukanda@gmail.com>
2025-09-13 11:19:09 +0300

    Update React.ts

--- 0x04-TypeScript/task_4/js/subjects/React.ts
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -1,1 +1,23 @@
+/// <reference path="Subject.ts" />
+/// <reference path="Teacher.ts" />
 
+namespace Subjects {
+  // Declaration merging - add experienceTeachingReact to Teacher interface
+  export interface Teacher {
+    experienceTeachingReact?: number;
+  }
+
+  export class React extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for React';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher.experienceTeachingReact && this.teacher.experienceTeachingReact > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return 'No available teacher';
+      }
+    }
+  }
+}
